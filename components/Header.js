c35060a40e00d16d603e7d0f367176f26ca047cd
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-01-30 21:29:46 -0500

    Add header login

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, GlobeAltIcon, Bars3Icon, UserCircleIcon, UserIcon } from '@heroicons/react/24/solid';
 
 export const Header = () => {
   return (
@@ -23,8 +23,16 @@ export const Header = () => {
         <MagnifyingGlassIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
 
-      {/* right */}
-      <div></div>
+      {/* Right - Login */}
+      <div className='flex items-center space-x-4 justify-end text-gray-500'>
+        <p className='hidden md:inline cursor-pointer'>Become a host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer' />
+
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+          <Bars3Icon className='h-6' />
+          <UserCircleIcon className='h-6' />
+        </div>
+      </div>
     </header>
   );
 };
