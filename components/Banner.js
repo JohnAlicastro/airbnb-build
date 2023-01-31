cbe9f15aa6e6d7d94c762dc55cc74c74a8b65128
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-01-30 21:42:39 -0500

    Add banner

---
+++ components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from 'next/image';
+
+export const Banner = () => {
+  return (
+    <div className='relative h-[300px] sm:h-[400px] lg:[500px] xl:h-[600px] 2xl:h-[700px]'>
+      <Image src='https://links.papareact.com/0fm' fill style={{ objectFit: 'cover' }} alt='' />
+      <div className='absolute top-1/2 w-full text-center'>
+        <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
+        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full'>{`I'm flexible`}</button>
+      </div>
+    </div>
+  );
+};
+
+// export default Banner
