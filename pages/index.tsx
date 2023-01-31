551423e46ad3424de91794610aec07f379ebe349
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-01-31 13:06:37 -0500

    Add explore nearby section

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,14 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull data from server - API endpoints */}
+        </section>
+      </main>
     </div>
   );
 }
