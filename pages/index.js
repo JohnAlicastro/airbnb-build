c6f8deebae4a9ab92604745bbeea27a43bd0a9e7
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-01-31 14:07:28 -0500

    Add cardsData staticProps

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { SmallCard } from '../components/SmallCard';
 
 // const inter = Inter({ subsets: ['latin'] });
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className=''>
       <Head>
@@ -24,16 +24,21 @@ export default function Home({ exploreData }) {
       <Banner />
 
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        {/* SmallCard section */}
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
-          {/* Pull data from server - API endpoints */}
-          {/* below we are destructuring the props passed into Home func above */}
+          {/* below we are destructuring the exploreData props passed into Home func above */}
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {exploreData.map(({ img, distance, location }) => (
               <SmallCard key={img} img={img} distance={distance} location={location} />
             ))}
           </div>
+        </section>
+
+        {/* MediumCard section */}
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
         </section>
       </main>
     </div>
@@ -43,5 +48,8 @@ export default function Home({ exploreData }) {
 // getting static props and sending into Home function above
 export async function getStaticProps() {
   const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').then((res) => res.json());
-  return { props: { exploreData } };
+
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT').then((res) => res.json());
+
+  return { props: { exploreData, cardsData } };
 }
