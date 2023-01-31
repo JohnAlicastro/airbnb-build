726d38aa5dc346edf4497857e9e09b820dc17f09
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-01-30 21:48:48 -0500

    Add banner button

--- components/Banner.js
+++ components/Banner.js
@@ -7,7 +7,7 @@ export const Banner = () => {
       <Image src='https://links.papareact.com/0fm' fill style={{ objectFit: 'cover' }} alt='' />
       <div className='absolute top-1/2 w-full text-center'>
         <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
-        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full'>{`I'm flexible`}</button>
+        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>{`I'm flexible`}</button>
       </div>
     </div>
   );
