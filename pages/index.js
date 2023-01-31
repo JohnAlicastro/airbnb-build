7d6b4a73de67836a63b60629f2edfa00579ac6e0
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-01-31 14:00:30 -0500

    Style SmallCard component grid layout

--- pages/index.js
+++ pages/index.js
@@ -29,9 +29,11 @@ export default function Home({ exploreData }) {
 
           {/* Pull data from server - API endpoints */}
           {/* below we are destructuring the props passed into Home func above */}
-          {exploreData.map(({ img, distance, location }) => (
-            <SmallCard key={img} img={img} distance={distance} location={location} />
-          ))}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData.map(({ img, distance, location }) => (
+              <SmallCard key={img} img={img} distance={distance} location={location} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
