7e8e4c97a50eee490408ee0f6ac65ea6d4878a80
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-02-01 15:53:16 -0500

    Add /search page buttons

--- pages/search.js
+++ pages/search.js
@@ -12,8 +12,12 @@ const Search = () => {
           <p className='text-xs'>300+ Stays for 5 number of guests</p>
           <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
 
-          <div className='hidden lg:inline-flex'>
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
             <p className='searchPageButton'>Cancellation Flexibility</p>
+            <p className='searchPageButton'>Type of Place</p>
+            <p className='searchPageButton'>Price</p>
+            <p className='searchPageButton'>Rooms and Beds</p>
+            <p className='searchPageButton'>More filters</p>
           </div>
         </section>
       </main>
