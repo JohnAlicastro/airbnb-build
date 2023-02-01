d444565f7394d3f6e893c6e752738c5ab8248380
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-02-01 12:04:33 -0500

    Add LargeCard styling

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -1,7 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
 
 export const LargeCard = ({ img, title, description, buttonText }) => {
-  return <div>LargeCard</div>;
+  return (
+    <section className='relative py-16 cursor-pointer'>
+      <div className='relative h-96 min-w-[300px]'>
+        <Image src={img} fill className='rounded-2xl' style={{ objectFit: 'cover' }} alt='' />
+      </div>
+
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+
+        <button>{buttonText}</button>
+      </div>
+    </section>
+  );
 };
 
 // export default LargeCard
