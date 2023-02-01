dafba7dfdcb8e6e91b24b723b865e94af30f30e5
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-02-01 12:22:36 -0500

    Populate and style Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>COMMUNITY</h5>
+        <p>This</p>
+        <p>Is</p>
+        <p>All</p>
+        <p>Placeholder</p>
+        <p>Text</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>HOST</h5>
+        <p>This</p>
+        <p>Is</p>
+        <p>All</p>
+        <p>Placeholder</p>
+        <p>Text</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
 };
 
 // export default Footer
