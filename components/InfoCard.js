381d2b5238033d2f979eed49a614c838bd7c8ac3
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-02-02 17:25:13 -0500

    Add InfoCard styling

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -5,7 +5,7 @@ import { HeartIcon } from '@heroicons/react/24/outline';
 
 export const InfoCard = ({ img, location, title, description, star, price, total }) => {
   return (
-    <div className='flex'>
+    <div className='flex py-7 pr-4 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t'>
       <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
         <Image src={img} fill style={{ objectFit: 'cover' }} alt='' />
       </div>
