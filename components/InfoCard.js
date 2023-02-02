be8d1401045d3a4b0044f3ae2a77b2a3f89e9c7e
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-02-02 17:10:06 -0500

    Add InfoCard image

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/24/solid';
+import { HeartIcon } from '@heroicons/react/24/outline';
 
 export const InfoCard = ({ img, location, title, description, star, price, total }) => {
-  return <div>InfoCard</div>;
+  return (
+    <div>
+      <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
+        <Image src={img} fill style={{ objectFit: 'cover' }} alt='' />
+      </div>
+
+      <div>
+        <div>
+          <p>{location}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // export default InfoCard
