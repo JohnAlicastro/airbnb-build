1efbc95240bb1728cd05d235c87594f716b9abfc
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-02-02 14:36:10 -0500

    Add display search query data on search page

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import { useRouter } from 'next/router';
+import { format } from 'date-fns';
 
 const Search = () => {
+  const router = useRouter();
+
+  const { location, startDate, endDate, numberOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), 'MMMM dd yyyy');
+  const formattedEndDate = format(new Date(endDate), 'MMMM dd yyyy');
+  const dateRange = `${formattedStartDate} - ${formattedEndDate}`;
+
   return (
     <div>
       <Header />
 
       <main className='flex'>
         <section className='flex-grow pt-14 px-6'>
-          <p className='text-xs'>300+ Stays for 5 number of guests</p>
-          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+          <p className='text-xs'>
+            300+ Stays -{dateRange}- for {numberOfGuests} guests
+          </p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
 
           <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
             <p className='searchPageButton'>Cancellation Flexibility</p>
