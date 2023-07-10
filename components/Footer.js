0cda3c6a03f8d5bc001fbee1f5871d856d78f9c8
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-07-10 16:52:59 -0400

    Update components/Footer.js info

--- components/Footer.js
+++ components/Footer.js
@@ -4,39 +4,39 @@ export const Footer = () => {
   return (
     <div className='grid grid-cols-1 gap-y-10 bg-gray-100 px-32 py-14 text-gray-600 md:grid-cols-4'>
       <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
-
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>COMMUNITY</h5>
-        <p>This</p>
-        <p>Is</p>
-        <p>All</p>
-        <p>Placeholder</p>
-        <p>Text</p>
-      </div>
-
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>HOST</h5>
-        <p>This</p>
-        <p>Is</p>
-        <p>All</p>
-        <p>Placeholder</p>
-        <p>Text</p>
-      </div>
-
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>SUPPORT</h5>
+        <h5 className='font-bold'>Support</h5>
         <p>Help Center</p>
-        <p>Trust & Safety</p>
-        <p>Say Hi YouTube</p>
-        <p>Easter Eggs</p>
-        <p>For the Win</p>
+        <p>AirCover</p>
+        <p>Supporting people with disabilities</p>
+        <p>Cancellation options</p>
+        <p>Our COVID-19 Response</p>
+        <p>Report a neighborhood concern</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Community</h5>
+        <p>Airbnb.org: disaster relief housing</p>
+        <p>Combating discrimination</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Hosting</h5>
+        <p>Airbnb your home</p>
+        <p>AirCover for Hosts</p>
+        <p>Explore hosting resources</p>
+        <p>Visit our community forum</p>
+        <p>How to host responsibly</p>
+        <p>Airbnb-friendly apartments</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Airbnb</h5>
+        <p>Newsroom</p>
+        <p>Learn about new features</p>
+        <p>Letter from our founders</p>
+        <p>Careers</p>
+        <p>Investors</p>
+        <p>Gift cards</p>
       </div>
     </div>
   );
