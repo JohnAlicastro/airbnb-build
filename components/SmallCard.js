0bd17e2c188c6ef0be2f1c848fdb1ff7979807dd
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-07-11 15:31:28 -0400

    Update components/SmallCard.js

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -4,12 +4,10 @@ import React from 'react';
 export const SmallCard = ({ img, location, distance }) => {
   return (
     <div className='m-2 mt-5 flex transform cursor-pointer items-center space-x-4 rounded-xl transition duration-200 ease-out hover:scale-105 hover:bg-gray-100'>
-      {/* Left - Image */}
       <div className='relative h-16 w-16'>
         <Image src={img} fill className='rounded-lg' alt='' />
       </div>
 
-      {/* Right - Info */}
       <div>
         <h2>{location}</h2>
         <h3 className='text-gray-500'>{distance}</h3>
